e727fc18f5329d0bd48c10a42b062da6edd79d7b
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2021-09-21 17:29:55 -0500

    first commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aviones{
+        public int id { get; set; }
+        public string marca { get; set; }
+        public int modelo { get; set; }
+        public int numero_asientos { get; set; }
+        public int numero_banos { get; set; }
+        public int capacidad_maxima { get; set; }
+    }
+} 
---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20210913164034_MigraInicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class MigraInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aviones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    modelo = table.Column<int>(type: "int", nullable: false),
+                    numero_asientos = table.Column<int>(type: "int", nullable: false),
+                    numero_banos = table.Column<int>(type: "int", nullable: false),
+                    capacidad_maxima = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aviones", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aviones");
+        }
+    }
+}
